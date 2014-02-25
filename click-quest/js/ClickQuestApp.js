16d950db8fdfb7ec744a71580863ab1b9cafa608
unknown <prc1544@prk-pricingtmp1.us.ad.hertz.com>
2014-02-25 10:32:15 -0500

    Put click-quest app in folder

---
+++ click-quest/js/ClickQuestApp.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var clickQuest = angular.module('clickQuestApp', ['ngResource']);
+
+/*clickQuest.config( function($routeProvider) {
+  $routeProvider.
+	when('/onRent', 
+		{templateUrl: 'views/onRents.html', controller: 'OnRentController'}).
+	otherwise({redirectTo: '/'});
+});*/
